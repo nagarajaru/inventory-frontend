b667b1a533572f94e6abf697f887138c53a69080
nagarajaru <nagarajarun64945@gmail.com>
2024-08-15 23:49:19 -0700

    changes

--- src/services/productServices.js
+++ src/services/productServices.js
@@ -13,7 +13,7 @@ const productServices = {
         });
     },
     viewProducts: async () => {
-        return await protectedInstance.get('/products');
+        return await protectedInstance.get('https://management-1-8wua.onrender.com/products');
     }
 }
 
